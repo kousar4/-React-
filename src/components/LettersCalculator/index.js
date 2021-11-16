42722b8beaf4dea78841f5cd8883108f6e6051a7
kousar <kousarnaazz@gmail.com>
2021-11-16 13:46:16 +0000

    first commit

--- src/components/LettersCalculator/index.js
+++ src/components/LettersCalculator/index.js
@@ -1,47 +1,50 @@
 import {Component} from 'react'
+
 import './index.css'
 
 class LettersCalculator extends Component {
   state = {
-    Input: '',
+    inputPhrase: '',
   }
 
-  onChangeInput = event => {
+  onChangeInputPhrase = event => {
     const {value} = event.target
-    this.setState({Input: value})
+
+    this.setState({inputPhrase: value})
   }
 
   render() {
-    const {Input} = this.state
-    return (
-      <div className="container">
-        <div className="cal-container">
-          <h1 className="heading">Calculate the Letters you enter</h1>
+    const {inputPhrase} = this.state
 
-          <label className="para" htmlFor="phraseText">
-            Enter the phrase
-          </label>
-          <input
-            type="text"
-            id="phraseText"
-            placeholder="Enter the phrase"
-            className="data"
-            onChange={this.onChangeInput}
-            value={Input}
-          />
-          <div className="output-container">
-            <h1 className="letter">No.of letters:{Input.length}</h1>
+    return (
+      <div className="app-container">
+        <div className="letters-calculator-container">
+          <div className="calculator-container">
+            <h1 className="heading">Calculate the Letters you enter</h1>
+            <div className="input-phrase-container">
+              <label className="label" htmlFor="phraseText">
+                Enter the phrase
+              </label>
+              <input
+                className="letters-input"
+                id="phraseText"
+                onChange={this.onChangeInputPhrase}
+                placeholder="Enter the phrase"
+                type="text"
+                value={inputPhrase}
+              />
+            </div>
+            <p className="letters-count">No.of letters: {inputPhrase.length}</p>
           </div>
-        </div>
-        <div className="pic-container">
           <img
+            alt="letters calculator"
+            className="letters-calculator-image"
             src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png"
-            alt="pic"
-            className="pic"
           />
         </div>
       </div>
     )
   }
 }
+
 export default LettersCalculator
